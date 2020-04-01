2f0ba32c55b77c3a4a483342cf01dde799c20e9c
Anderson0017 <andeerson0017@gmail.com>
2020-04-01 15:51:54 -0300

    Authentication middleware

--- src/routes.js
+++ src/routes.js
@@ -13,10 +13,16 @@ const { Router } = require('express')
 
 const routes = new Router();
 
+/* Controller */
 const UserController = require('./app/controller/UserController');
 const SessionController = require('./app/controller/SessionController');
 
-routes.post('/users', UserController.store);
+/* Middlewares */
+const authMiddleware = require('./app/middlewares/auth')
+
 routes.post('/sessions', SessionController.store)
+routes.post('/users', UserController.store);
+
+routes.put('/users', authMiddleware, UserController.update);
 
 module.exports = routes;
