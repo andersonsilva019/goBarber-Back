6bfd18b16deeca9788049f5764dd7cd8b1b881d8
Anderson0017 <andeerson0017@gmail.com>
2020-04-06 14:29:24 -0300

    Configurando templates de e-mail

--- src/lib/Mail.js
+++ src/lib/Mail.js
@@ -1,5 +1,8 @@
 const nodeMailer = require('nodemailer');
 const mailConfig = require('../config/mail');
+const exphbs = require('express-handlebars');
+const nodemailerhbs = require('nodemailer-express-handlebars');
+const { resolve } = require('path');
 
 class Mail {
   constructor() {
@@ -13,6 +16,8 @@ class Mail {
       auth: auth.user ? auth : null,
 
     })
+
+    this.configureTemplates();
   }
 
   sendMail(message) {
@@ -20,6 +25,21 @@ class Mail {
       ...mailConfig.default,
       ...message,
     })
+  }
+
+  configureTemplates() {
+    const viewPath = resolve(__dirname, '..', 'app', 'views', 'emails');
+
+    this.transporter.use('compile', nodemailerhbs({
+      viewEngine: exphbs.create({
+        layoutsDir: resolve(viewPath, 'layouts'),
+        partialsDir: resolve(viewPath, 'partials'),
+        defaultLayout: 'default',
+        extname: '.hbs',
+      }),
+      viewPath,
+      extName: '.hbs'
+    }))
   }
 }
 
